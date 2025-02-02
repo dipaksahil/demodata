567b4ac6edf02405b9cb2a46b05a3ec4c88c2b14
Deepak <er.deepaksati@gmail.com>
2025-02-02 11:10:31 +0530

    my first commit

---
+++ src/test/java/com/demogit/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
